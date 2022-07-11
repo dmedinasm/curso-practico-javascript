832ecc3b5a6f05cf3e51560a4bbda1707275a72e
Daniel Miguel Medinas Mateo <dmedinasm@gmail.com>
2022-07-10 22:17:28 -0400

    Reto: Matematicas Javascript, validar que el triangulo existe

--- retocalculofiguras.js
+++ retocalculofiguras.js
@@ -1,24 +1,26 @@
 const alturaIsosceles = (lado1, lado2, lado3) => {
-    if (lado1 === lado2){
+    if (lado1 + lado2 <= lado3 || lado2 + lado3 <= lado1 || lado1 + lado3 <= lado2 ){
+        return ("El triangulo no existe. Inserte nuevas medidas");
+    } else if (lado1 === lado2){
         return Math.sqrt((lado1 ** 2) - (lado3 ** 2)/4 );
     } else if (lado1 === lado3){
         return Math.sqrt((lado1 ** 2) - (lado2 ** 2)/4 );
     } else if (lado2 === lado3){
         return Math.sqrt((lado2 ** 2) - (lado1 ** 2)/4 );
-    }else{
+    } else{
         return ("El triangulo no es isosceles. Inserte nuevas medidas");
     }
 }
 
 const calcularAlturaIsosceles = () => {
     const lado1 = document.getElementById("InputLado1");
-    const value1 = lado1.value;
+    const value1 = parseFloat(lado1.value);
     
     const lado2 = document.getElementById("InputLado2");
-    const value2 = lado2.value;
+    const value2 = parseFloat(lado2.value);
 
     const lado3 = document.getElementById("InputLado3");
-    const value3 = lado3.value;
+    const value3 = parseFloat(lado3.value);
     const altura = alturaIsosceles(value1, value2, value3);
     alert(altura);
 }
