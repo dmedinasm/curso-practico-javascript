9638bcce4d057e04e78f8a613a1f357b1f8f150e
Daniel Miguel Medinas Mateo <dmedinasm@gmail.com>
2022-07-12 23:27:49 -0400

    Taller 2: Precios con descuentos.Escribir  HTML con Javascript

--- descuentos.js
+++ descuentos.js
@@ -7,6 +7,18 @@ const calcularPrecioConDescuento = (precio, descuento) => {
     return precioConDescuento;
 }
 
+const onClickButtonPriceDiscount = () => {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText =`El precio con descuento es de: $ ${precioConDescuento}`;
+}
 
 
 
