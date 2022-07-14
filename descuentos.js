889d83f9102c13c22f3636a101adb44ecedac8ab
Daniel Miguel Medinas Mateo <dmedinasm@gmail.com>
2022-07-13 23:31:18 -0400

    Taller 2: Reto. Funciones mas inteligentes

--- descuentos.js
+++ descuentos.js
@@ -11,13 +11,40 @@ const onClickButtonPriceDiscount = () => {
     const inputPrice = document.getElementById("InputPrice");
     const priceValue = inputPrice.value;
 
-    const inputDiscount = document.getElementById("InputDiscount");
-    const discountValue = inputDiscount.value;
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
 
-    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+    coupons = [
+        {
+            name: "JuanDC_es_Batman",
+            discount: 15,
+        },
+        {
+            name: "pero_no_le_digas_a_nadie",
+            discount: 30,
+        },
+        {
+            name: "es_un_secreto",
+            discount: 25,
+        },
+    ];
+
+    const isCouponValueValid = function(coupon) {
+        return coupon.name === couponValue;
+    };
+
+    const userCoupon = coupons.find(isCouponValueValid);
+    
+    if(!userCoupon){
+        alert(`El cupon ${couponValue} no es valido`)
+    }else{
+        const descuento = userCoupon.discount;
+        const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+    
 
     const resultP = document.getElementById("ResultP");
-    resultP.innerText =`El precio con descuento es de: $ ${precioConDescuento}`;
+    resultP.innerText = "El precio con descuento es de:" + precioConDescuento ;
+    }
 }
 
 
