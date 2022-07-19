6ea898423f1b1cc3d3fef0bb3adccf85b78a8c4f
Daniel Miguel Medinas Mateo <dmedinasm@gmail.com>
2022-07-18 23:52:12 -0400

    Taller 3: Reto Encapsular en funcion el calculo de la mediana

--- mediana.js
+++ mediana.js
@@ -1,23 +1,45 @@
 
-const calcularMediaAritmetica = (lista) => {
-const sumaLista = lista.reduce(
-    function(valorAcumulado = 0, nuevoElemento){
-        return valorAcumulado + nuevoElemento;
-    }
-)
-
-const promedioLista = sumaLista /lista.length;
-
-return promedioLista;
-}
 const lista1 = [
     100,
     200,
+    50,
+    700,
     500,
     400000000,
 ]
 
-const mitadLista1 = parseInt(lista1.length / 2);
+function ordenarLista(lista) {
+const listaOrdered = lista.sort(
+    (num1, num2) => {
+        if(num1 === num2) {
+        return 0; 
+        }
+
+        if(num1 < num2) {
+        return -1;
+
+        }else{
+        return 1;
+        }
+})
+    for(let i = 0; i < lista.length; i++){
+        
+    }
+ return listaOrdered;
+}
+
+
+const calcularMediaAritmetica = (lista) => {
+    const sumaLista = lista.reduce(
+        function(valorAcumulado = 0, nuevoElemento){
+            return valorAcumulado + nuevoElemento;
+        }
+    )
+    
+    const promedioLista = sumaLista /lista.length;
+    
+    return promedioLista;
+    }
 
 const esPar = (numerito) =>{
     if (numerito % 2 === 0){
@@ -27,7 +49,12 @@ const esPar = (numerito) =>{
     }
 }
 
+
+function calcularMediana(lista) {
 let mediana;
+
+const lista1 = ordenarLista(lista);
+const mitadLista1 = parseInt(lista1.length / 2);
 
 if (esPar(lista1.length)){
     const elemento1 = lista1[mitadLista1 - 1];
@@ -40,3 +67,8 @@ if (esPar(lista1.length)){
 }else{
     mediana = lista1[mitadLista1];
 }
+
+return mediana;
+}
+
+console.log(calcularMediana(lista1));
