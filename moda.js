0ca017c20fbe57a39beb9ab205e78418ed2f257c
Daniel Miguel Medinas Mateo <dmedinasm@gmail.com>
2022-07-21 00:11:25 -0400

    Taller 3: Reto: Encapsular calculo de la Moda en una funcion

--- moda.js
+++ moda.js
@@ -1,34 +1,24 @@
-const lista1 = [
-    1,
-    2,
-    3,
-    1,
-    2,
-    3,
-    4,
-    2,
-    2,
-    2,
-    1,
-];
 
-const lista1Count = {};
+const calcularModa = (lista) => {
+ const listaCount = {};
 
-lista1.map(
+ if (Array.isArray(lista)){
+  lista.map(
     function(elemento){
-        if(lista1Count[elemento]){
-       lista1Count[elemento] += 1;
-    }else{
-    lista1Count[elemento] = 1;
-    }
-}) 
+    listaCount[elemento] ? listaCount[elemento] += 1 : listaCount[elemento] = 1;
+    })
+}else{
+  [];
+} 
 
-const lista1Array = Object.entries(lista1Count).sort(
+const listaArray = Object.entries(listaCount).sort(
     function (elementoA, elementoB) {
        return elementoA[1] - elementoB[1];
     }
-)  //El metodo Object.entries devuelve un array con los valores de una lista de objetos
+)
+  //El metodo Object.entries devuelve un array con los valores de una lista de objetos
+const moda = listaArray[listaArray.length - 1];
 
-const moda = lista1Array[lista1Array.length - 1];
+return moda;
+}
 
-console.log(moda);
